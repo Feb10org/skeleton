7bbb709d48a504692cdf9b894ab5d7531b4c70d4
Piotr Obolewicz <e-proc@gft.com>
2025-02-10 16:39:02 +0100

    Commit test

--- src/test/java/abc/skeleton/SkeletonApplicationTests.java
+++ src/test/java/abc/skeleton/SkeletonApplicationTests.java
@@ -1,5 +1,6 @@
 package abc.skeleton;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class SkeletonApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
 	}
 
 }
