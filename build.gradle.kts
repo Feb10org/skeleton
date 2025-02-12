e9854ca9f71fcbd744fd6adeaac6151320b82bbb
marcingft <marcin.formela@gft.com>
2025-02-12 13:14:07 +0100

    add json log formatter for spring profile - json-logs

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	//    implementation("com.microsoft.sqlserver:mssql-jdbc:12.8.1.jre11")
+	implementation("net.logstash.logback:logstash-logback-encoder:8.0")
 
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
