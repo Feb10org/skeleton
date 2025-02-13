f14b90870421b2b6f0c4391631ace34dfc9179a7
marcingft <marcin.formela@gft.com>
2025-02-13 15:33:27 +0100

    add OpenTelemetry

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,6 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("net.logstash.logback:logstash-logback-encoder:8.0")
 
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
