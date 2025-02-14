c1420acbecd2e80354b95a00b1cf93b660ee2c68
Piotr Obolewicz <e-proc@gft.com>
2025-02-14 08:46:05 +0100

    cucumber package added

--- src/main/java/abc/skeleton/cucumber/user/UserController.java
+++ src/main/java/abc/skeleton/cucumber/user/UserController.java
@@ -2,19 +2,22 @@ package abc.skeleton.cucumber.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
-    
+
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
-    
+
     @PostMapping("/register")
     public ResponseEntity<User> registerUser(@RequestBody User user) {
         User registeredUser = userService.registerUser(user.getUsername(), user.getPassword());
