0e3a7c58238d6473ec0c9855788fa4760cfc04fb
Piotr Sieniawski <piotr.sieniawski@gft.com>
2025-02-19 15:32:09 +0100

    FLyway migration added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,52 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.4.2"
-	id("io.spring.dependency-management") version "1.1.7"
+    java
+    id("org.springframework.boot") version "3.4.2"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.flywaydb.flyway") version "11.3.3"
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.flywaydb:flyway-sqlserver:11.3.3")
+        classpath("com.microsoft.sqlserver:mssql-jdbc:12.8.1.jre11")
+    }
 }
 
 group = "abc"
 version = "0.0.1-SNAPSHOT"
 
+
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+//    implementation("org.flywaydb:flyway-core")
+//    implementation("org.flywaydb:flyway-sqlserver")  // Required for Azure SQL Edge
+//    implementation("com.microsoft.sqlserver:mssql-jdbc:12.8.1.jre11")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+
+flyway {
+	url = "jdbc:sqlserver://localhost:1433;databaseName=skeleton_db;encrypt=false"
+    user = "skeleton"
+    password = "skele@Ton123"
+    locations = arrayOf("filesystem:src/db/migration")
 }
--- docker/db/README.md
+++ docker/db/README.md
@@ -21,6 +21,14 @@ docker compose start
 ### DB initialization script
 DB is being initialized by the [setup.sql](./sql/setup.sql) file
 
+### Schema creation/migration with flyway
+From project's root folder run
+```
+./gradlew flywayMigrate -i
+```
+ 
+
+
 ### Connection details 
  - URL: jdbc:sqlserver://localhost:1433;database=<DB_NAME>
  - db name and credentials: check [.env](./.env) file
