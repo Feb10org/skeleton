6f97874c98b9fbab1f19768a538d5b4eca4c7d8d
Grzegorz Fula <grzegorz.fula@gft.com>
2025-02-20 12:41:47 +0100

    rest client - add trace id

--- src/main/java/abc/skeleton/rest_client/config/CorrelationIdInterceptor.java
+++ src/main/java/abc/skeleton/rest_client/config/CorrelationIdInterceptor.java
@@ -1,5 +1,6 @@
 package abc.skeleton.rest_client.config;
 
+import org.slf4j.MDC;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
@@ -7,13 +8,19 @@ import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 
 import java.io.IOException;
-import java.util.UUID;
+
+import static abc.skeleton.rest_client.config.RequestHandler.TRACE_ID;
+import static abc.skeleton.rest_client.config.RequestHandler.TRACE_ID_HEADER;
 
 public class CorrelationIdInterceptor implements ClientHttpRequestInterceptor {
+
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
-        HttpHeaders headers = request.getHeaders();
-        headers.add("X-Correlation-ID", UUID.randomUUID().toString()); // FIXME value from MDC here if available?
+        if (MDC.get(TRACE_ID) != null && !MDC.get(TRACE_ID).isBlank()) {
+            HttpHeaders headers = request.getHeaders();
+            headers.add(TRACE_ID_HEADER, MDC.get(TRACE_ID));
+        }
+
         return execution.execute(request, body);
     }
 }
