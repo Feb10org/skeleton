0001d3b9bc5e50370708d7470ebb7a735a7755f8
psieniawski <piotr.sieniawski@gmail.com>
2025-02-20 12:27:59 +0100

    Local DB
    * Local DB WIP
    
    * Fixed user password to work on x86
    
    * Local DB WIP
    
    * Local DB Done
    
    * Local DB Done
    
    * FLyway migration added
    
    * FLyway migration added
    
    ---------
    
    Co-authored-by: Piotr Sieniawski <e-prik@gft.com>
    Co-authored-by: Piotr Sieniawski <piotr.sieniawski@gft.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,51 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.4.2"
-	id("io.spring.dependency-management") version "1.1.7"
+    java
+    id("org.springframework.boot") version "3.4.2"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.flywaydb.flyway") version "11.3.3"
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.flywaydb:flyway-sqlserver:11.3.3")
+        classpath("com.microsoft.sqlserver:mssql-jdbc:12.8.1.jre11")
+    }
 }
 
 group = "abc"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	//    implementation("com.microsoft.sqlserver:mssql-jdbc:12.8.1.jre11")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	implementation("org.springframework.boot:spring-boot-starter")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+
+flyway {
+	url = "jdbc:sqlserver://localhost:1433;databaseName=skeleton_db;encrypt=false"
+    user = "skeleton"
+    password = "skele@Ton123"
+    locations = arrayOf("filesystem:src/db/migration")
 }
