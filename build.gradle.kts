81148a6ea8a2d73d7b06a39e66d4bae034f06c8d
mar-konczak <mkonczak1@gmail.com>
2025-02-21 14:07:12 +0100

    openapi (#6)
    * openapi
    
    * openapi

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+
 val jacksonDatabindNullableVersion = "0.2.6"
 val restAssuredVersion = "5.5.0"
 val logstashVersion = "8.0"
@@ -45,6 +47,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
     implementation("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
     //    implementation("com.microsoft.sqlserver:mssql-jdbc:12.8.1.jre11")
     implementation("org.openapitools:jackson-databind-nullable:$jacksonDatabindNullableVersion")
@@ -53,7 +56,28 @@ dependencies {
     testImplementation("io.rest-assured:rest-assured:$restAssuredVersion")
 }
 
-openApiGenerate {
+tasks.named<JavaCompile>("compileJava") {
+    dependsOn("openApiGenerateClient", "openApiGenerateServer")
+}
+sourceSets["main"].java.srcDir("${layout.buildDirectory.asFile.get()}/generated/src/main/java")
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+flyway {
+	url = "jdbc:sqlserver://localhost:1433;databaseName=skeleton_db;encrypt=false"
+    user = "skeleton"
+    password = "skele@Ton123"
+    locations = arrayOf("filesystem:src/db/migration")
+}
+
+val apiBasePackage = "com.sample"
+val generatedDirPath = "$buildDir/generated"
+val apiFile = "$rootDir/resource/api.yml"
+
+
+tasks.register<GenerateTask>("openApiGenerateClient"){
     generatorName.set("java")
     inputSpec.set("$rootDir/src/main/resources/remote_apis/petstore_api.yaml")
     outputDir.set("${layout.buildDirectory.asFile.get()}/generated")
@@ -72,19 +96,16 @@ openApiGenerate {
     generateModelTests.set(false)
 }
 
-tasks.named<JavaCompile>("compileJava") {
-    dependsOn("openApiGenerate")
-}
-sourceSets["main"].java.srcDir("${layout.buildDirectory.asFile.get()}/generated/src/main/java")
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
-
-flyway {
-	url = "jdbc:sqlserver://localhost:1433;databaseName=skeleton_db;encrypt=false"
-    user = "skeleton"
-    password = "skele@Ton123"
-    locations = arrayOf("filesystem:src/db/migration")
+tasks.register<GenerateTask>("openApiGenerateServer"){
+    generatorName.set("spring")
+    inputSpec.set(apiFile)
+    outputDir.set(generatedDirPath)
+    apiPackage.set("$apiBasePackage.api")
+    invokerPackage.set("$apiBasePackage.invoker")
+    modelPackage.set("$apiBasePackage.model")
+    configOptions.set(mapOf(
+        "interfaceOnly" to "true",
+        "skipDefaultInterface" to "true",
+        "useSpringBoot3" to "true",
+    ))
 }
